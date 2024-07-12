4967b1d96c085aa9ed1510f78dd404c59ab27143
WillianGomes949 <williangomes949@gmail.com>
2024-07-12 14:47:24 -0300

    pelo amor de deus

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,6 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Inicio from './Components/paginas/inicio/Inicio';
 import NovoVideo from './Components/paginas/novoVideo/NovoVideo';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function AppRoutes() {
   return (
