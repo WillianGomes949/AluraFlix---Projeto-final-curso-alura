d969deaf1bbdb93a1e2456bf2f529817d744bd54
WillianGomes949 <williangomes949@gmail.com>
2024-07-12 15:02:10 -0300

    desisto

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Inicio from './Components/paginas/inicio/Inicio';
-import NovoVideo from './Components/paginas/novoVideo/NovoVideo';
+import Inicio from './Inicio';
+import NovoVideo from './NovoVideo';
 
 export default function AppRoutes() {
   return (
