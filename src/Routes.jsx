5553ddcb61dae01f180a46a3a68b6517b594ed87
WillianGomes949 <williangomes949@gmail.com>
2024-07-12 16:24:54 -0300

    ...

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -6,10 +6,8 @@ export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/novoVideo" element={<NovoVideo />} />
-        </Route>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/novoVideo" element={<NovoVideo />} />
       </Routes>
     </BrowserRouter>
   );
