63bc06eda644d5eb33b95b5224adc28bcdfb1831
WillianGomes949 <williangomes949@gmail.com>
2024-07-12 14:51:42 -0300

    aahhhhh

--- src/Components/paginas/inicio/Inicio.js
+++ src/Components/paginas/inicio/Inicio.js
@@ -1,7 +1,8 @@
-import Header from '@/Components/header/Header';
-import Banner from '@/Components/banner/Banner';
-import Footer from '@/Components/footer/Footer';
-import SectionVideos from '@/Components/sectionVideos/SectionVideos';
+import Banner from "@/Components/banner/Banner";
+import Footer from "@/Components/footer/Footer";
+import Header from "@/Components/header/Header";
+import SectionVideos from "@/Components/sectionVideos/SectionVideos";
+
 
 export default function Inicio() {
   return (
--- src/Components/paginas/novoVideo/NovoVideo.js
+++ src/Components/paginas/novoVideo/NovoVideo.js
@@ -1,6 +1,6 @@
-import Footer from '@/Components/footer/Footer';
-import Formulario from '@/Components/formulario/Formulario';
-import Header from '@/Components/header/Header';
+import Footer from "@/Components/footer/Footer";
+import Formulario from "@/Components/formulario/Formulario";
+import Header from "@/Components/header/Header";
 
 export default function NovoVideo() {
   return (
