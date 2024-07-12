be32bd06e08102d1991d06b55769f13d2e386842
WillianGomes949 <williangomes949@gmail.com>
2024-07-12 16:15:53 -0300

    ...

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Inicio from './Inicio';
-import NovoVideo from './NovoVideo';
+import Inicio from './app/pages/Inicio';
+import NovoVideo from './app/pages/NovoVideo';
 
 export default function AppRoutes() {
   return (
